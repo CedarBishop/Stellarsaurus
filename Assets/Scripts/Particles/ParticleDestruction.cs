936eaba2d8a280b0f0a51fcd579ebd7beb2850f3
Max <madmanmax120@hotmail.com>
2020-05-03 20:50:43 +1000

    Updated Explosive Barrel

--- Assets/Scripts/Particles/ParticleDestruction.cs
+++ Assets/Scripts/Particles/ParticleDestruction.cs
@@ -8,13 +8,6 @@ public class ParticleDestruction : MonoBehaviour
 
     public void DestroyParticle()
     {
-        StartCoroutine(DestructionSequence());
-    }
-
-    // Call this to destroy the particle upon starting to destroy it when it finishes.
-    IEnumerator DestructionSequence()
-    {
-        yield return new WaitForSeconds(lifetime);
-        Destroy(gameObject);
+        Destroy(gameObject, lifetime);
     }
 }
