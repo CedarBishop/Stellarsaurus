8d27b2bcf9979da872f4776d503de6a51e4f0804
Max <madmanmax120@hotmail.com>
2020-05-09 20:54:27 +1000

    Created Destructable Windows + Particles
    TODO: Create script editor to edit size of the window.
    
    Completed: Windows block objects EXCEPT for bullets, which damage them and will pass through. When the window's health reaches 0 however, it'll destroy itself and play some particles.

--- Assets/Scripts/Environment/Window.cs
+++ Assets/Scripts/Environment/Window.cs
@@ -6,11 +6,50 @@ public class Window : MonoBehaviour
 {
     [SerializeField] private GameObject windowSegment;
     [SerializeField] private int windowHeight = 1;
+    [SerializeField] private ParticleSystem glassParticleSystem;
 
     public int health = 3;
 
+    private void Awake()
+    {
+        glassParticleSystem = transform.GetComponentInChildren<ParticleSystem>();
+    }
+
     void OnValidate()
     {
-        
+        // Add/Remove additional Window Segment
+        // Reposition Window Segment
+        // Update Trigger Box Bounds
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Projectile"))
+        {
+            // Update the parant window health with how much this window segment just got damaged by
+            UpdateHealth(collision.GetComponent<Projectile>().damage);
+        }     
+    }
+
+    public void UpdateHealth(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+            DeathSequence();
+    }
+
+    private void DeathSequence()
+    {
+        // Play particle system
+        glassParticleSystem.Play();
+
+        // Set timer to delete particle system
+        glassParticleSystem.GetComponent<ParticleDestruction>().DestroyParticle();
+
+        // Move particle system outside of this window
+        glassParticleSystem.transform.parent = null;
+
+        // Delete gameobject.
+        Destroy(gameObject);
     }
 }
