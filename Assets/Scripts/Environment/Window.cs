052f824bcd8427a8b14b32a26abddf4ed4ad4cf5
Max <madmanmax120@hotmail.com>
2020-05-09 17:32:05 +1000

    Working on Windows + changed game's icon

---
+++ Assets/Scripts/Environment/Window.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour
+{
+    [SerializeField] private GameObject windowSegment;
+    [SerializeField] private int windowHeight = 1;
+
+    public int health = 3;
+
+    void OnValidate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Environment/WindowSegment.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowSegment : MonoBehaviour
+{
+    private Window window;
+
+    private void Awake()
+    {
+        window = transform.GetComponentInParent<Window>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Projectile"))
+        {
+            window.health -= collision.damage;
+        }
+    }
+}
