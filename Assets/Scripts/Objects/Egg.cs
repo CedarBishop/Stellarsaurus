c69fbb9ddc9d05cab0be6fe0bc54c6181bfcc0a2
CedarBishop <cedar.bishop@outlook.com>
2020-06-30 23:00:38 +1000

    Created egg and started creating carrier AI

---
+++ Assets/Scripts/Objects/Egg.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Egg : MonoBehaviour
+{
+    AIType aiTypeToHatch;
+    bool hasHatched;
+    public void Init (AIType ai)
+    {
+        aiTypeToHatch = ai;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform"))
+        {
+            HatchUnit();
+        }
+    }
+
+    void HatchUnit ()
+    {
+        if (hasHatched)
+        {
+            return;
+        }
+
+        hasHatched = true;
+        AI ai = Instantiate(LevelManager.instance.aiPrefab, transform.position, Quaternion.identity);
+        ai.Initialise(aiTypeToHatch);
+        Destroy(gameObject);
+    }    
+}
