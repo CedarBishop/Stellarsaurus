67c4a8e9a170a2261da64d5bc7c0742a8703f2d2
Cedar Bishop <c_bishop@wargaming.net>
2020-07-14 18:57:40 +1000

    Updated Ghost grab script and created interfaces for what can be grabbed and interacted with

--- Assets/Scripts/Player/GhostGrab.cs
+++ Assets/Scripts/Player/GhostGrab.cs
@@ -7,228 +7,63 @@ public class GhostGrab : MonoBehaviour
     [HideInInspector] public int playerNumber;
 
     public Transform objectOriginTransform;
-    public SpriteRenderer objectSprite;
 
     [HideInInspector] public Player player;
-    [HideInInspector] public AimType aimType;
-    [HideInInspector] public bool isGamepad;
 
-    private GameObject heldObject;
     private Camera mainCamera;
     private GhostMovement ghostMovement;
-    private CameraShake cameraShake;
-
-    bool isAimingRightstick;
-
+    private IGhostGrabbable grabbable;
+    private IGhostInteractable interactable;
+    private IGhostGrabbable heldObject;
 
 
     void Start()
     {
         mainCamera = Camera.main;
         ghostMovement = GetComponent<GhostMovement>();
-        cameraShake = mainCamera.GetComponent<CameraShake>();
     }
-
-    private void Update()
-    {
-        switch (aimType)
-        {
-            case AimType.FreeAim:
-                if (isGamepad == false)
-                {
-                    Vector2 direction = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-                    objectOriginTransform.right = direction;
-                }
-                break;
-            case AimType.EightDirection:
-                if (isGamepad == false)
-                {
-                    Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    Vector2 directionToTarget = target - new Vector2(transform.position.x, transform.position.y);
-                    objectOriginTransform.right = TranslateToEightDirection(directionToTarget.normalized);
-                }
-                break;
-            case AimType.FourDirection:
-
-                //switch (playerMovement.GetDirection())
-                //{
-                //    case Orthogonal.Up:
-                //        gunOriginTransform.right = Vector2.up;
-                //        gunSprite.flipY = false;
-                //        break;
-                //    case Orthogonal.Right:
-                //        gunOriginTransform.right = Vector2.right;
-                //        gunSprite.flipY = false;
-                //        break;
-                //    case Orthogonal.Down:
-                //        gunOriginTransform.right = Vector2.down;
-                //        gunSprite.flipY = true;
-                //        break;
-                //    case Orthogonal.Left:
-                //        gunOriginTransform.right = new Vector2(-1, 0.01f);
-                //        gunSprite.flipY = true;
-                //        break;
-                //    default:
-                //        break;
-                //}
-                break;
-            case AimType.HybridEightDirection:
-                //if (isGamepad == false)
-                //{
-                //    Vector2 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //    Vector2 directionToTarget = target - new Vector2(transform.position.x, transform.position.y);
-                //    gunOriginTransform.right = TranslateToEightDirection(directionToTarget.normalized);
-                //}
-                break;
-
-            default:
-                break;
-        }
-
-    }
-
-    private void LateUpdate()
-    {
-        isAimingRightstick = false;
-    }
-
-    public void Aim(Vector2 v, bool isRightstick = false)
-    {
-        if (isAimingRightstick && isRightstick == false)
-        {
-            return;
-        }
-        isAimingRightstick = isRightstick;
-        switch (aimType)
-        {
-            case AimType.FreeAim:
-                if (isGamepad)
-                {
-                    if (Mathf.Abs(v.x) > 0.5f || Mathf.Abs(v.y) > 0.5f)
-                    {
-                        objectOriginTransform.right = v;
-                    }
-                }
-                break;
-            case AimType.EightDirection:
-                if (isGamepad)
-                {
-                    if (Mathf.Abs(v.x) > 0.5f || Mathf.Abs(v.y) > 0.5f)
-                    {
-                        objectOriginTransform.right = TranslateToEightDirection(v);
-                    }
-                }
-                break;
-            case AimType.FourDirection:
-                break;
-
-            case AimType.HybridEightDirection:
-
-                if (Mathf.Abs(v.x) > 0.5f || Mathf.Abs(v.y) > 0.5f)
-                {
-                    objectOriginTransform.right = TranslateToEightDirection(v);
-                }
-
-                break;
-            default:
-                break;
-        }
-
-    }
-
-    Vector2 TranslateToEightDirection(Vector2 v)
-    {
-        Vector2 result = v;
-
-        if (Mathf.Abs(v.x) < 0.25f && Mathf.Abs(v.y) > 0.25f)
-        {
-            // Up & Down
-            if (v.y > 0)
-            {
-                result = Vector2.up;
-                objectSprite.flipY = false;
-            }
-            else
-            {
-                result = Vector2.down;
-                objectSprite.flipY = true;
-            }
-        }
-        else if (Mathf.Abs(v.x) > 0.25f && Mathf.Abs(v.y) < 0.25f)
-        {
-            // Left & Right
-            if (v.x > 0)
-            {
-                result = Vector2.right;
-                objectSprite.flipY = false;
-            }
-            else
-            {
-                result = new Vector2(-1, 0.01f);
-                objectSprite.flipY = true;
-            }
-        }
-        else if (Mathf.Abs(v.x) > 0.25f && Mathf.Abs(v.y) > 0.25f)
-        {
-            // Diagonals
-            if (v.x < -0.25f && v.y < -0.25f)
-            {
-                // down left
-                result = new Vector2(-1, -1);
-                objectSprite.flipY = true;
-            }
-            else if (v.x > 0.25f && v.y < -0.25f)
-            {
-                // down right
-                result = new Vector2(1, -1);
-                objectSprite.flipY = false;
-            }
-            else if (v.x > 0.25f && v.y > 0.25f)
-            {
-                // up right
-                result = new Vector2(1, 1);
-                objectSprite.flipY = false;
-            }
-            else if (v.x < -0.25f && v.y > 0.25f)
-            {
-                // up left
-                result = new Vector2(-1, 1);
-                objectSprite.flipY = true;
-            }
-
-        }
-        else
-        {
-            result = transform.right;
-        }
-
-
-        return result;
-    }
-
+  
     private void OnTriggerStay2D(Collider2D other)
     {
-
-    }
-
-    public void SetAimType(AimType _AimType)
-    {
-        aimType = _AimType;
+        interactable = other as IGhostInteractable;        
+        grabbable = other as IGhostGrabbable;
     }
 
     public void Grab ()
     {
-        if (heldObject == null)
+        if (interactable != null)
+        {
+            interactable.GhostInteract();
+        }
+        if (heldObject != null)
         {
             Drop();
             return;
+        }
+        else
+        {
+            if (grabbable != null)
+            {
+                grabbable.Grab();
+                heldObject = grabbable;
+            }
         }
     }
 
     public void Drop ()
     {
-
-
+        heldObject.Drop();
         heldObject = null;
     }
+}
+
+public interface IGhostInteractable 
+{
+    void GhostInteract ();
+}
+
+public interface IGhostGrabbable
+{
+    void Grab();
+    void Drop();
 }
