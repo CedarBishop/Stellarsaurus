329fcb4ab00155d0900c5a7a6ee3ffb4284b0fec
CedarBishop <cedar.bishop@outlook.com>
2020-07-17 21:32:00 +1000

    Finished logic for climb levels

--- Assets/Scripts/ClimbCamera.cs
+++ Assets/Scripts/ClimbCamera.cs
@@ -6,19 +6,27 @@ public class ClimbCamera : MonoBehaviour
 {
     public float climbSpeed;
 
+    public float timeBeforeStarting;
     public int towerHeight;
     public Grid bottomPiece;
     public Grid[] middlePieces;
     public Grid topPiece;
 
+    private bool isClimbing;
+
     private void Start()
     {
         GenerateTower();
+        StartCoroutine("StartClimbing");
     }
 
     private void FixedUpdate()
     {
         if (transform.position.y >= (towerHeight * 10) + 5)
+        {
+            return;
+        }
+        if (isClimbing == false)
         {
             return;
         }
@@ -35,5 +43,11 @@ public class ClimbCamera : MonoBehaviour
         }
         
         Instantiate(topPiece, new Vector3(0, towerHeight * 10, 0), Quaternion.identity);
+    }
+
+    IEnumerator StartClimbing ()
+    {
+        yield return new WaitForSeconds(timeBeforeStarting);
+        isClimbing = true;
     }
 }
---
+++ Assets/Scripts/ClimbTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClimbTrigger : MonoBehaviour
+{
+    private bool playerWon;
+    void Start()
+    {
+        playerWon = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (playerWon)
+        {
+            return;
+        }
+
+        if (collision.GetComponent<PlayerMovement>())
+        {
+            if (GameManager.instance.SelectedGamemode != null)
+            {
+                GameManager.instance.SelectedGamemode.PlayerWonRound(collision.GetComponent<PlayerMovement>().playerNumber);
+            }
+            playerWon = true;
+        }
+    }
+}
