3877138994980f72ce42c864eea21bc573ed4c9e
Ivan <karyakinivan@live.com>
2020-07-22 02:53:35 +1000

    Added a new MainMenu Iteration

---
+++ Assets/Scripts/TeleporterListMain.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterListMain : MonoBehaviour
+{
+    public GameObject[] toBattle = new GameObject [4], original = new GameObject[4];
+}
