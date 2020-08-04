e70f7022f88d5045f42dfb18294d896c37baaa6a
CedarBishop <cedar.bishop@outlook.com>
2020-08-04 23:30:52 +1000

    Created Disintegratable script for platforms to disintigrate a few seconds after being landed on

---
+++ Assets/Scripts/Objects/Disintegratable.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disintegratable : MonoBehaviour
+{
+    public string[] objectTags;
+    public float timeTillDestroy;
+    public float animationTime;
+
+    private bool startedDisintegrating;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string collisionTag = collision.collider.tag;
+        
+        foreach (var tag in objectTags)
+        {
+            if (collisionTag == tag)
+            {
+                if (startedDisintegrating == false)
+                {
+                    if (collision.collider.transform.position.y - transform.position.y > 0)
+                    {
+                        startedDisintegrating = true;
+                        StartCoroutine("Disintegrate");
+                    }                    
+                }
+            }
+        }
+    }
+
+    IEnumerator Disintegrate()
+    {
+        yield return new WaitForSeconds(timeTillDestroy);
+        Destroy(gameObject);
+
+    }
+
+}
