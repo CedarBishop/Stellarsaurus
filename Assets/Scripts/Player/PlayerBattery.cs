4a784f0feb9ee1cf38f9823de15fda71aa6b0534
CedarBishop <cedar.bishop@outlook.com>
2020-08-10 20:05:17 +1000

    Wrote player battery script

---
+++ Assets/Scripts/Player/PlayerBattery.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerBattery : MonoBehaviour
+{
+    private int playerNumber;
+    private int health;
+    private Image batteryImage;
+    private Material batteryMaterial;
+
+    public Sprite[] healthSprites;
+
+    public void Initialise (int playerNum)
+    {
+        playerNumber = playerNum;
+        health = 3;
+        batteryImage.GetComponent<Image>();
+        batteryMaterial = batteryImage.material;
+        batteryMaterial.SetColor("PlayerColor", GameManager.instance.playerColours[playerNumber - 1]);
+    }
+
+    public void UpdateHealth (int value)
+    {
+        health = value;
+        batteryImage.sprite = healthSprites[health - 1];
+        batteryMaterial.SetFloat("HealthValue", health);
+    }
+}
