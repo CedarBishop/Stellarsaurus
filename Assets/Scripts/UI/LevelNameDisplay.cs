ed7ddfac38f4d6ca0c580177a420a4dc159ea2e8
Ivan <karyakinivan@live.com>
2020-08-11 14:10:32 +1000

    added script to display level name and added IK_MainMenu to the build setting

---
+++ Assets/Scripts/UI/LevelNameDisplay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelNameDisplay : MonoBehaviour
+{
+    public Text levelNameText;
+    void Start()
+    {
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+        levelNameText.text = sceneName;
+    }
+}
