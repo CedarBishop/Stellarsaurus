5a28ca4fe1c68f60abd15294f1c9db20a18aee3b
CedarBishop <cedar.bishop@outlook.com>
2020-08-15 20:55:45 +1000

    made exit game script and fixed up build setting and camera bounds of main menu

---
+++ Assets/Scripts/Environment/ExitTeleporter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTeleporter : MonoBehaviour
+{
+    ParticleSystem[] particles;
+    private void Start()
+    {
+        particles = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerMovement>())
+        {
+            particles[0].Play();
+            Application.Quit();
+        }
+    }
+}
