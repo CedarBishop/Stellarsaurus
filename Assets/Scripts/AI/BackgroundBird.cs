37111ee981c2f125daa6bee50600831bc0fea5f9
CedarBishop <cedar.bishop@outlook.com>
2020-08-23 11:22:14 +1000

    Created background bird AI

---
+++ Assets/Scripts/AI/BackgroundBird.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlatformerPathFinding;
+
+public class BackgroundBird : MonoBehaviour
+{
+	public Transform[] targets;
+    public float movementSpeed;
+
+    private Transform target;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        FindNewPath();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        CheckIfReachedTarget();
+    }
+
+    void Move()
+    {
+        if (target == null)
+        {
+            return;
+        }
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.Translate(direction * movementSpeed * Time.fixedDeltaTime);
+        spriteRenderer.flipX = (direction.x < 0);
+    }
+
+    void CheckIfReachedTarget()
+    {
+        if (Vector2.Distance(target.position, transform.position) < 0.1f)
+        {
+            FindNewPath();
+        }
+    }
+
+    void FindNewPath()
+    {
+        if (targets == null)
+        {
+            return;
+        }
+        target = targets[Random.Range(0, targets.Length)];
+    }
+}
