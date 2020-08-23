8bbc0e97373b8ef0a2f6e35071841b1e0c7333c5
CedarBishop <cedar.bishop@outlook.com>
2020-08-23 22:34:40 +1000

    Updated the glow shader and mine script to make the mine pusalte when active

--- Assets/Scripts/Weapons/Mine.cs
+++ Assets/Scripts/Weapons/Mine.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Mine : Explosive
 {
-    bool isSet = false;
+    private bool isSet = false;
+    private Material material;
+
     public override void InitExplosive(float explodeTime, float explodeSize, int _Damage, int _PlayerNumber, float force, float cameraShakeDuration, float cameraShakeMagnitude, float cookTime = 0)
     {
         explosionSize = explodeSize;
@@ -16,12 +18,19 @@ public class Mine : Explosive
         magnitude = cameraShakeMagnitude;
         rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.AddForce(transform.right * initialForce);
+        material = GetComponent<SpriteRenderer>().material;
+        material.SetColor("_Color",Color.white);
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Platform") || collision.gameObject.CompareTag("Wall"))
         {
+            if (isSet == false)
+            {
+                StartCoroutine("Pulsate");
+            }
             isSet = true;
             rigidbody.bodyType = RigidbodyType2D.Kinematic;
             rigidbody.velocity = Vector2.zero;
@@ -39,6 +48,18 @@ public class Mine : Explosive
         if (collision.GetComponent<PlayerMovement>() || collision.GetComponent<AI>())
         {
             Explode();
+        }
+    }
+
+    IEnumerator Pulsate ()
+    {
+        material.SetColor("_Color", Color.red);
+        while (true)
+        {
+            material.SetFloat("_EmissionScaler", 1.0f);
+            yield return new WaitForSeconds(1f);
+            material.SetFloat("_EmissionScaler", 2.0f);
+            yield return new WaitForSeconds(0.2f);
         }
     }
 }
